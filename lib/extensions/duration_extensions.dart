3560aaefe4dc95d3c8cdeea4db88fc49ad0f691a
3vmartinet <mofx71@gmail.com>
2024-11-02 21:53:41 +0100

    hms() formatter doesn't skip minutes by default

--- lib/extensions/duration_extensions.dart
+++ lib/extensions/duration_extensions.dart
@@ -3,7 +3,7 @@ const _emptyPrefix = "00:";
 extension DurationExtensions on Duration {
   String _twoDigits(int n) => n.toString().padLeft(2, '0');
 
-  String hms({bool skipHours = true, bool skipMinutes = true}) {
+  String hms({bool skipHours = true, bool skipMinutes = false}) {
     final String hours = _twoDigits(inHours);
     final String minutes = _twoDigits(inMinutes.remainder(60));
     final String seconds = _twoDigits(inSeconds.remainder(60));
