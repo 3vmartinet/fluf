3b8c069e6a9180c915ace7f9e853d13f4155ff23
3vmartinet <mofx71@gmail.com>
2024-11-02 20:09:52 +0100

    Extend Duration.hms() API, add unit test

--- lib/extensions/duration_extensions.dart
+++ lib/extensions/duration_extensions.dart
@@ -1,11 +1,26 @@
+const _emptyPrefix = "00:";
+
 extension DurationExtensions on Duration {
   String _twoDigits(int n) => n.toString().padLeft(2, '0');
 
-  String hms() {
+  String hms({bool skipHours = true, bool skipMinutes = true}) {
     final String hours = _twoDigits(inHours);
     final String minutes = _twoDigits(inMinutes.remainder(60));
     final String seconds = _twoDigits(inSeconds.remainder(60));
+    var hms = "$hours:$minutes:$seconds";
 
-    return "$hours:$minutes:$seconds";
+    if (skipHours && hms.startsWith(_emptyPrefix)) {
+      hms = _removeZeroPrefix(hms);
+
+      if (skipMinutes && hms.startsWith(_emptyPrefix)) {
+        return _removeZeroPrefix(hms);
+      } else {
+        return hms;
+      }
+    } else {
+      return hms;
+    }
   }
+
+  String _removeZeroPrefix(String hms) => hms.replaceFirst(_emptyPrefix, "");
 }
---
+++ test/duration_extensions_test.dart
@@ -0,0 +1,48 @@
+import 'package:fluf/extensions/duration_extensions.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('HMS formatting removes empty hours and minutes', () {
+    expect(
+      "01:01:01",
+      const Duration(hours: 1, minutes: 1, seconds: 1).hms(),
+    );
+
+    expect(
+      "01:01:01",
+      const Duration(hours: 1, minutes: 1, seconds: 1)
+          .hms(skipHours: false, skipMinutes: false),
+    );
+
+    expect(
+      "01:01",
+      const Duration(hours: 0, minutes: 1, seconds: 1).hms(),
+    );
+
+    expect(
+      "01:01",
+      const Duration(hours: 0, minutes: 1, seconds: 1).hms(skipMinutes: false),
+    );
+
+    expect(
+      "00:01:01",
+      const Duration(hours: 0, minutes: 1, seconds: 1).hms(skipHours: false),
+    );
+
+    expect(
+      "01",
+      const Duration(hours: 0, minutes: 0, seconds: 1).hms(),
+    );
+
+    expect(
+      "00:00:01",
+      const Duration(hours: 0, minutes: 0, seconds: 1)
+          .hms(skipHours: false, skipMinutes: false),
+    );
+
+    expect(
+      "01",
+      const Duration(hours: 0, minutes: 0, seconds: 1).hms(),
+    );
+  });
+}
