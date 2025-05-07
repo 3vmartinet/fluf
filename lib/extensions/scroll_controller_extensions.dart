48aac76dc2ef47a2b2e64dede2b718347b0687a2
valentin.martinet <valentin.martinet@>
2025-05-07 17:04:12 +0200

    Check if ScrollController has clients before reading position

--- lib/extensions/scroll_controller_extensions.dart
+++ lib/extensions/scroll_controller_extensions.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/widgets.dart';
 
 extension ScrollControllerExtensions on ScrollController {
-  double get progress =>
-      position.hasContentDimensions ? offset / position.maxScrollExtent : 0;
+  double get progress {
+    if (!hasClients) return 0;
+
+    return position.hasContentDimensions
+        ? offset / position.maxScrollExtent
+        : 0;
+  }
 }
