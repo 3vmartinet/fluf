91a98908539b33792bdf32a9687b6489ec98cc37
valentin.martinet <valentin.martinet@...>
2026-03-13 13:31:11 +0100

    Add optional custom tranformation to image generation

--- lib/extensions/render_repaint_boundary_extensions.dart
+++ lib/extensions/render_repaint_boundary_extensions.dart
@@ -9,11 +9,16 @@ extension RenderRepaintBoundaryExtensions on RenderRepaintBoundary {
     required String directoryPath,
     required String filename,
     ui.ImageByteFormat format = ui.ImageByteFormat.png,
+    Uint8List Function(Uint8List)? customTransform,
     double pixelRatio = 1.0,
   }) async {
     final image = await toImage(pixelRatio: pixelRatio);
     final bytes = await image.toByteData(format: format);
-    final buffer = bytes?.buffer.asUint8List() ?? Uint8List(0);
+    var buffer = bytes?.buffer.asUint8List() ?? Uint8List(0);
+
+    if (customTransform != null) {
+      buffer = customTransform(buffer);
+    }
 
     final targetFile = File('$directoryPath/$filename');
     final output = await targetFile.writeAsBytes(buffer.toList(), flush: true);
